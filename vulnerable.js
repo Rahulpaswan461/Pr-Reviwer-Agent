220db4d6d12127c058a102fd2872fd86a0275c9d
Rahul Paswan <rahulpaswan@aegiscovenant.com>
2026-09-12 15:42:32 +0530

    added a fix

--- vulnerable.js
+++ vulnerable.js
@@ -1,6 +1,6 @@
 APIPromise.get("/user",async(req,res)=>{
     const result = await db.query(
-        `SELECT * FROM users WHERE id = ${req.query.id} OR name=${req.query.name}`
+        `SELECT * FROM users WHERE id = ${req.query.id} OR age = ${req.query.age}%`
     )
     res.json(result.rows)
 })
